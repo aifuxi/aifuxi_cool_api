1de9c997204f47aca21e32b39676a1fef2b30f33
aifuxi <aifuxi.js@gmail.com>
2023-08-24 21:42:10 +0800

    refactor: 优化main.go

--- main.go
+++ main.go
@@ -10,23 +10,31 @@ import (
 	"api.aifuxi.cool/settings"
 )
 
-var err error
-
 func main() {
-	err = settings.Init()
+	err := Init()
 	if err != nil {
-		log.Fatalf("初始化配置失败: %v", err)
+		log.Fatalf("初始化失败: %v\n", err)
+	}
+
+	r := routers.Init()
+	addr := fmt.Sprintf("localhost:%d", settings.AppConfig.Port)
+	r.Run(addr)
+}
+
+func Init() error {
+	err := settings.Init()
+	if err != nil {
+		fmt.Printf("初始化配置失败: %v\n", err)
+		return err
 	}
 
 	logger.Init()
 
 	err = mysql.Init()
 	if err != nil {
-		log.Fatalf("连接 MySQL 失败: %v", err)
+		fmt.Printf("连接 MySQL 失败: %v\n", err)
+		return err
 	}
 
-	r := routers.Init()
-
-	addr := fmt.Sprintf("localhost:%d", settings.AppConfig.Port)
-	r.Run(addr)
+	return nil
 }
