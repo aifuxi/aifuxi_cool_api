058f02bbec0b864e758be7cd0bae9e9ee0afde34
aifuxi <aifuxi.js@gmail.com>
2023-08-24 22:07:34 +0800

    feat: 上传文件后返回全路径地址

--- controller/admin_upload.go
+++ controller/admin_upload.go
@@ -3,6 +3,8 @@ package controller
 import (
 	"fmt"
 	"path/filepath"
+	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,10 +14,10 @@ var baseUploadDir = "uploads"
 func UploadFile(c *gin.Context) {
 	// 单文件
 	file, _ := c.FormFile("file")
-	dst := filepath.Join(baseUploadDir, file.Filename)
+	filename := fmt.Sprintf("%d-%s", time.Now().Unix(), strings.ToLower(file.Filename))
+	dst := filepath.Join(baseUploadDir, filename)
 	c.SaveUploadedFile(file, dst)
 
-	fileUrl := fmt.Sprintf("/%s/%s", baseUploadDir, file.Filename)
-
+	fileUrl := fmt.Sprintf("http://%s/%s/%s", c.Request.Host, baseUploadDir, filename)
 	ResponseOk(c, fileUrl)
 }
